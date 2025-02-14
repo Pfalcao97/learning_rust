bbce14febbb3c55ea9712292ef861f78ffb51b3a
Pedro Falcao <pedrofalcao97@gmail.com>
2025-02-13 23:24:02 -0300

    create basic clap cli

---
+++ geometry/src/main.rs
@@ -0,0 +1,35 @@
+use clap::{Parser, Subcommand};
+
+const PI:f64 = 3.1415;
+
+/// A CLI to calculate geometrical shapes properties
+#[derive(Debug, Parser)]
+#[command(name = "geometry")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// The Square shape
+    #[command(arg_required_else_help = true)]
+    Square { side: f64 },
+
+    /// The Circle shape 
+    #[command(arg_required_else_help = true)]
+    Circle {radius: f64 }    
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::Square { side } => {
+            println!("Square of area {:?}", side*side);
+        }
+        Commands::Circle {radius} => {
+		println!("Circle of area {:?}", PI*radius.powf(2.0))
+	}
+    }
+}
